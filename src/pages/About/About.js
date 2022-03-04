2145dd9420d43e4afa52407dd0ff844fb0ce1d9d
ClockWize171 <jupiterminhtin998@gmail.com>
2022-03-04 18:25:13 +0630

    About Page adjustment

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,17 +1,27 @@
 import React from "react";
-import { Link, Text, Box, Container, Heading } from "@chakra-ui/react";
+import { Link, Text, Box, Container, Heading, useMediaQuery } from "@chakra-ui/react";
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 const About = (props) => {
+
+    // Screen Size
+    const [isNotSmallerScreen] = useMediaQuery("(min-width:479px)");
+
     return (
         <Container padding={5}>
-            <Box paddingTop={100} textAlign="center">
+            <Box
+                paddingTop={isNotSmallerScreen ? 100 : 5}
+                textAlign="center">
                 <Heading
                     color="blue.400"
                     align="center"
                     as="u">About</Heading>
             </Box>
-            <Box textAlign="justify" lineHeight={10} paddingTop={5}>
+            <Box
+                textAlign="justify"
+                lineHeight={10}
+                paddingTop={5}
+                paddingBottom={isNotSmallerScreen ? 50 : 10}>
                 <Text fontSize="xl" style={{ textIndent: "4rem" }} fontWeight="semibold">
                     This web app is developed with ReactJS. All the information of countries are being fetched and rendered on the <Link fontWeight="bold" color='blue.400' onClick={() => window.open("https://restcountries.com/")}>REST Countries<ExternalLinkIcon mx={2} /></Link>and you can donate to help a countries RESTful API alive.
                 </Text>
